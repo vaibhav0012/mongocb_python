35a608a6c88dcb49082fbcccfd2c92067f96936b
vaibhav0012 <vaibhavmehrao12@gmail.com>
2024-09-08 00:16:08 +0530

    Added Updated tox file

--- setup.py
+++ setup.py
@@ -40,6 +40,6 @@ setup(
     },
     package_dir={"": "src"},
     packages=find_packages(where="src"),
-    install_requires=get_requirements("./requirements_dev.txt"),
+    install_requires=get_requirements("./requirements.txt"),
     
 )
